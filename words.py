13fa0a2aee5792f4d24952c2e376b30a302380da
Dominik <dodosmon@gmial.com>
2020-06-15 18:24:22 +0200

    Initial commit

---
+++ words.py
@@ -0,0 +1,16 @@
+words_list = (
+    'kot',
+    'abisynia',
+    'kroplówka',
+    'śmiech',
+    'drzewopochodne',
+    'plastik',
+    'skocznie',
+    'celuloza',
+    'hydroliza',
+    'przekładnia',
+    'reduktor',
+    'pamięć'
+    'kreatyna',
+    'paliwo'
+)
